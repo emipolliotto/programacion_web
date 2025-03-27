9c7b79b19b231799f4983989ef2a334b16e8a143
emipolliotto <epolliotto@itba.edu.ar>
2025-03-27 19:36:55 -0300

    clase 3

---
+++ ejercicios_clase1.js
@@ -0,0 +1,49 @@
+//ejercicio1:
+//cambia
+let ciudad1 = 'salta'
+let pais1 = 'argentina'
+let edad1 = 19
+console.log(ciudad1,pais1,edad1)
+edad1 = 20
+console.log(edad1)
+
+//no cambia
+const ciudad2 = 'salta'
+const pais2 = 'argentina'
+const edad2 = 19
+console.log(ciudad2,pais2,edad2)
+edad1 = 20
+console.log(edad2)
+
+
+//ejercicio2:
+let valor1 = 12
+let valor2 = 15
+let valor3 = 24
+
+console.log((valor1+valor2+valor3)/3)
+console.log((valor1+valor2+valor3)%2)
+
+//ejercicio3:
+let coche = {
+    marca :'toyota',
+    modelo:'nose',
+    anio: 2023,
+}
+console.log(typeof coche.marca)
+
+//ejercicio4:
+let ciudades = ['valencia','paris','londres','napoles','milan']
+console.log(ciudades)
+ciudades[2]='salta'
+console.log(ciudades)
+
+//ejercicio5:
+let peliculas=[{titulo1:'peli1',director1:'director1',anio1:'anio1'},
+    {titulo2:'peli2',director2:'director2',anio2:'anio2'},
+    {titulo3:'peli3',director3:'director3',anio3:'anio3'}]
+
+console.log(peliculas[2].director3)
+
+//ejercicio6:
+
