4f40f7ce67f0242816453aee173b9b9924b6f686
emipolliotto <epolliotto@itba.edu.ar>
2025-03-29 10:59:40 -0300

    clase 3

--- ejercicios_clase1.js
+++ ejercicios_clase1.js
@@ -46,4 +46,78 @@ let peliculas=[{titulo1:'peli1',director1:'director1',anio1:'anio1'},
 console.log(peliculas[2].director3)
 
 //ejercicio6:
+let var1 = true
+let var2 = false
+let var3 = false
 
+console.log(var1 || var2)
+console.log(var1 && var3)
+
+//ejercicio7:
+let num1 = 34
+let num2 = 12
+let num3 = 7
+
+let resultado = (num2 < num1) && (num1 < num3)
+console.log(resultado)
+
+//ejercicio8:
+let nombre = 'emilia'
+let apellido = 'polliotto'
+
+let juntos = nombre + ' ' + apellido
+console.log(juntos)
+
+//ejercicio9:
+let numero = 34
+console.log(numero)
+numero++
+console.log(numero)
+
+numero-- 
+console.log(numero)
+
+//ejercicio10:
+let estudiante = {
+    nombre: 'emilia',
+    edad: 19,
+    carrera: 'negocios',
+}
+
+console.log(estudiante.edad)
+console.log(estudiante.carrera)
+console.log(estudiante.nombre)
+
+//ejercicio11:
+let numeros = [[1,2,3],[4,5,6],[7,8,9]]
+console.log(numeros[0][1])
+
+//ejecicio12:
+let numero1 = 23
+let numero2 = 57
+
+console.log(numero1 < numero2)
+console.log(numero1 <= numero2)
+console.log(numero1 > numero2)
+console.log(numero1 >= numero2)
+console.log(numero1 == numero2)
+console.log(numero1 === numero2)
+console.log(numero1 != numero2)
+console.log(numero1 !== numero2)
+
+//ejercicio13:
+let cualquiera = ''
+
+let cualquiera1 = null
+let cualquiera2 = null
+
+console.log(cualquiera)
+console.log(cualquiera1)
+console.log(cualquiera2)
+
+//ejercicio14:
+let numstring = '123'
+let cambiado2 = Number(numstring)
+
+let resultados = cambiado2 + 23
+console.log(resultados)
