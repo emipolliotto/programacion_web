2e7a45d5f5fb417f461870fd5eac2b2b70f5cdb4
emipolliotto <epolliotto@itba.edu.ar>
2025-04-04 10:21:52 -0300

    clase 5

---
+++ integracion/script.js
@@ -0,0 +1,9 @@
+document.getElementById('nombre')
+console.log(document.getElementById('nombre').value)
+
+//class.nombredelaclass
+//id#nombredelid
+
+document.getElementsByClassName('qcy')
+document.querySelector('#saludar')
+document.querySelector('.elemLista')
